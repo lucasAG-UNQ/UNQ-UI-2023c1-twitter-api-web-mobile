d5d16046eb1a4f84cd205d8bd5a1ac23ea3676b3
Gabriel <g.r.maugeri@gmail.com>
2023-04-08 16:26:19 -0300

    GET - /tweet/{id}  esta funcionando

--- Api/src/main/kotlin/org/unqui/controlers/TweetController.kt
+++ Api/src/main/kotlin/org/unqui/controlers/TweetController.kt
@@ -1,7 +1,9 @@
 package org.unqui.controlers
 
+import io.javalin.http.BadRequestResponse
 import io.javalin.http.Context
 import io.javalin.http.UnauthorizedResponse
+import org.unq.TweetException
 import org.unq.TwitterSystem
 import org.unqui.dtos.TweetsResultDTO
 import org.unqui.mappers.TweetMapper
@@ -36,7 +38,23 @@ class TweetController(var twitterSystem: TwitterSystem) {
     }
 
     fun postTweet(ctx: Context) { ctx.result("TODO") }
-    fun getTweet(ctx: Context) { ctx.result("TODO") }
+    fun getTweet(ctx: Context) {
+        try {
+            val token = ctx.header("Authorization")
+            JwtController().validate(token as String)
+            val tweet = twitterSystem.getTweet( ctx.pathParam("id") as String)
+            val tweetDTO =  TweetMapper(twitterSystem).tweetToTweetDTO(tweet)
+            ctx.status(200)
+            ctx.json(tweetDTO)
+        }
+        catch (e: NotFoundToken){
+            throw UnauthorizedResponse("Token not found")
+        }
+        catch (e: TweetException){
+            throw BadRequestResponse("No se encontró e Tweet")
+        }
+    }
+
     fun addLike(ctx: Context) { ctx.result("TODO") }
     fun retweet(ctx: Context) { ctx.result("TODO") }
     fun replay(ctx: Context) { ctx.result("TODO") }
--- Api/src/main/kotlin/org/unqui/mappers/TweetMapper.kt
+++ Api/src/main/kotlin/org/unqui/mappers/TweetMapper.kt
@@ -3,6 +3,7 @@ package org.unqui.mappers
 import org.unq.Tweet
 import org.unq.TwitterSystem
 import org.unqui.dtos.SimpleTweetDTO
+import org.unqui.dtos.TweetDTO
 
 class TweetMapper(var twitterSystem: TwitterSystem) {
     fun listTweetToListSimpleTweetDTO(tweets: MutableList<Tweet>) : MutableList<SimpleTweetDTO>{
@@ -24,6 +25,17 @@ class TweetMapper(var twitterSystem: TwitterSystem) {
             tweet.replies.size,
             tweet.reTweets.size,
             UserMapper(twitterSystem).listUserToListSimpleUserDTO(tweet.likes) )
+    }
+
+    fun tweetToTweetDTO(tweet: Tweet): TweetDTO{
+        return TweetDTO(tweet.id,
+                        TweetTypeDTOMapper(twitterSystem).toTwitTypeDTO(tweet.type),
+                        UserMapper(twitterSystem).userToSimpleUserDTO(tweet.user),
+                        tweet.content,
+                        tweet.date.toString(),
+                        TweetMapper(twitterSystem).listTweetToListSimpleTweetDTO(tweet.replies),
+                        TweetMapper(twitterSystem).listTweetToListSimpleTweetDTO(tweet.reTweets),
+                        UserMapper(twitterSystem).listUserToListSimpleUserDTO(tweet.likes))
     }
 
 }
