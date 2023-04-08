d9f8552c88b49e08cb527bd943837d8eb82f0b78
LeaMM <leatex_99@yahoo.com>
2023-04-08 20:46:41 -0300

    fixed getUser and findUserToLogin

--- Api/src/main/kotlin/org/unqui/controlers/UserController.kt
+++ Api/src/main/kotlin/org/unqui/controlers/UserController.kt
@@ -8,6 +8,7 @@ import org.unq.User
 import org.unq.UserException
 import org.unqui.dtos.TweetsResultDTO
 import org.unqui.dtos.UserLoginDTO
+import org.unqui.dtos.UsersResultDTO
 import org.unqui.mappers.TweetMapper
 import org.unqui.mappers.UserMapper
 
@@ -31,56 +32,50 @@ class UserController(private val twitterSystem: TwitterSystem, private val jwtCo
     }
 
     private fun findUserToLogin(userDTO: UserLoginDTO): User {
-        return twitterSystem.users.find { user -> user.username == userDTO.username } ?: throw UserException("Invalid username or password")
+        return twitterSystem.users.find { user -> user.username == userDTO.username && user.password == userDTO.password } ?: throw UserException("Invalid username or password")
     }
 
     fun register(ctx: Context) { ctx.result("TODO") }
 
     fun getUser(ctx: Context) {
         try {
-            val twitterUser: User = twitterSystem.getUser(ctx.pathParam("id"))
+            val twitterUser: User = ctx.attribute<User>("user")!!
             val userDTO =  UserMapper(twitterSystem).userToUserDTO(twitterUser)
             ctx.status(200)
             ctx.json(userDTO)
         }
-        catch (e: UserException){
+        catch (e: UserException){ // creo que no es necesatio este handling
             throw BadRequestResponse("Invalid user ID")
         }
     }
 
     fun getFollowingTweets(ctx: Context) {
         try {
-            val token = ctx.header("Authorization")
-            val id = JwtController().validate(token as String)
-            val listTweet = twitterSystem.getFollowingTweets(id)
+            val twitterUser: User = ctx.attribute<User>("user")!!
+            val listTweet = twitterSystem.getFollowingTweets(twitterUser.id)
             val listSimplTweetDTO = TweetMapper(twitterSystem).listTweetToListSimpleTweetDTO(listTweet.toMutableList())
             val tweetReuslt = TweetsResultDTO(listSimplTweetDTO)
             ctx.status(200)
             ctx.json(tweetReuslt)
-        }
-        catch (e: NotFoundToken){
-            throw UnauthorizedResponse("Token not found")
         }
         catch (e: TweetException){
             throw BadRequestResponse("No se encontró el Usuario")
         }
     }
 
-    fun getUsersToFollow(ctx: Context) { try {
-        val token = ctx.header("Authorization")
-        val id = JwtController().validate(token as String)
-        val listUsers = twitterSystem.getUsersToFollow(id)
-        val listSimplUserDTO = UserMapper(twitterSystem).listUserToListSimpleUserDTO(listUsers.toMutableList())
-        val userReuslt = UsersResultDTO(listSimplUserDTO)
-        ctx.status(200)
-        ctx.json(userReuslt)
+    fun getUsersToFollow(ctx: Context) {
+        try {
+            val twitterUser: User = ctx.attribute<User>("user")!!
+            val listUsers = twitterSystem.getUsersToFollow(twitterUser.id)
+            val listSimplUserDTO = UserMapper(twitterSystem).listUserToListSimpleUserDTO(listUsers.toMutableList())
+            val userReuslt = UsersResultDTO(listSimplUserDTO)
+            ctx.status(200)
+            ctx.json(userReuslt)
+        }
+        catch (e: TweetException){
+            throw BadRequestResponse("No se encontró el Usuario")
+        }
     }
-    catch (e: NotFoundToken){
-        throw UnauthorizedResponse("Token not found")
-    }
-    catch (e: TweetException){
-        throw BadRequestResponse("No se encontró el Usuario")
-    } }
 
     fun followUser(ctx: Context) { ctx.result("TODO") }
 }
