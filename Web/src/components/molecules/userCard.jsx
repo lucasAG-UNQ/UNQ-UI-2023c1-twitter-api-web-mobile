c7de97da8ae0cb64c30b9620f4c876abd22cb525
LeaTex <leatex_99@yahoo.com>
2023-05-18 01:32:41 -0300

    user card with some styles

--- Web/src/components/molecules/userCard.jsx
+++ Web/src/components/molecules/userCard.jsx
@@ -1,30 +1,94 @@
-import React from "react";
-import './userCard.css';
+import React, { useState, useEffect } from "react";
+import TwApi from "../services";
+import "./userCard.css";
 
 const UserCard = (user) => {
-  return (
-    <>
-    <div className="card mb-2">
-    <img src={user.backgroundImage} alt="Background" className="card-img-top" />
-        <div className="card-body">
-        <div className="profile">
-            <img src={user.image} alt="Perfil" className="rounded-lg" />
-        </div>
-        <h5 className="card-title">{user.username}</h5>
-      
+    const [loggedUser, setLoggedUser] = useState()
+    const [isLoggedUser, setIsLoggedUser] = useState(false)
+    const [isFollowingUser, setIsFollowingUser] = useState(false);
 
-                
-                <h6 className="mb-0">Seguidores</h6>
-                <span>{user.followers.length}</span>
+    const buttonFollowText = isFollowingUser ? 'Dejar de seguir' : 'Seguir'
+    const buttonFollowClass = isFollowingUser ? 'btn btn-outline-danger' : 'btn btn-outline-primary'
 
-                <h6 className="mb-0">Siguiendo</h6>
-                <span>{user.following.length}</span>
+    useEffect(() => {
+        TwApi.getLoggedUser()
+            .then((response) => {
+                setLoggedUser(response.data);
+                setIsLoggedUser(response.data.id === user.id);
+                setIsFollowingUser(response.data.following.some((usr) => usr.id === user.id));
+            })    
+    }, [])
+    
+    const handleClickFollow = () => {
+        TwApi.followUser(user.id)
+                .then( response => {
+                    setIsFollowingUser(!isFollowingUser)})
+                .catch((err) => {
+                    console.log(err.description) })
+    }
 
-                <h6 className="mb-0">Twitts</h6>
-                <span>{user.tweets.length}</span>
-                </div></div>
-    </>
-  );
+    if (!loggedUser) return <div>Loading... </div>;
+
+    return (
+        <>
+            <div className="container mb-3 justify-content-center">
+                <div className="profile-card">
+                    <img
+                        src={user.backgroundImage}
+                        alt="Background"
+                        className="profile-background"
+                    />
+
+                    <div className="d-flex align-items-center gap-2">
+                        <div className="profile-avatar">
+                            <img
+                                src={user.image}
+                                alt="Perfil"
+                                className="img-fluid rounded"
+                            />
+                        </div>
+
+                        <div>
+                            <h2 className="fw-bold f-27">{user.username}</h2>
+                        </div>
+                        <div className="d-flex justify-content-center gap-4">
+                            <div>
+                                <span className="d-block text-center fw-bold f-24">
+                                    {user.followers.length}
+                                </span>
+                                <span className="d-block text-center f-18">
+                                    Seguidores
+                                </span>
+                            </div>
+                            <div>
+                                <span className="d-block text-center fw-bold f-24">
+                                    {user.following.length}
+                                </span>
+                                <span className="d-block text-center f-18">
+                                    Siguiendo
+                                </span>
+                            </div>
+                            <div>
+                                <span className="d-block text-center fw-bold f-24">
+                                    {user.tweets.length}
+                                </span>
+                                <span className="d-block text-center f-18">
+                                    Twitts
+                                </span>
+                            </div>
+                        </div>
+                        <div className="follow">
+                            {isLoggedUser ? null : 
+                                <button className={buttonFollowClass} onClick={handleClickFollow}>
+                                    {buttonFollowText}
+                                </button>
+                            }
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default UserCard;
--- Web/src/components/pages/user.jsx
+++ Web/src/components/pages/user.jsx
@@ -1,15 +1,48 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
+import TwitLog from "../organisms/twitLog";
+import UserCard from "../molecules/userCard";
+import TwApi from "../services";
+import { useEffect } from "react";
 
 const User = () => {
-  const  { id } = useParams();
+    const { id } = useParams();
 
-  return (
-    <>
-      <h1>Usuario</h1>
-      <div className="usuario">Usuario ID: {id}</div>
-    </>
-  )
-}
+    // const loggedUser = JSON.parse(localStorage.getItem('twitterLoggedUser'))
+    const [user, setUser] = useState();
+    const [twitts, setTwitts] = useState([]);
+    const [error, setError] = useState("");
 
-export default User
+    useEffect(() => {
+        TwApi.getUser(id)
+            .then((response) => {
+                setUser(response.data);
+                setTwitts(response.data.tweets);
+            })
+            .catch((err) => {
+                setError(err.description);
+            });
+    }, [id]);
+
+    if (error)
+        return (
+            <>
+                <h2>Ups... algo salió mal</h2>
+                <p className="etiquetaRoja">{error}</p>
+            </>
+        );
+    if (!twitts || !user) return <div>Loading... </div>;
+
+    return (
+        <>
+            <section>
+                <UserCard {...user} />
+            </section>
+            <section>
+                <TwitLog twits={twitts} />
+            </section>
+        </>
+    );
+};
+
+export default User;
